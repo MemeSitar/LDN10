8f17eae3d315501e7cdc25eccf9237e166c45fac
MemeSitar <jurij.sitar@mjau.si>
2023-04-22 22:37:06 +0200

    Spremenil test.
    Testno metodologijo malo spremenil - vsaka funkcionalnost v svoji metodi.

--- Testing.java
+++ Testing.java
@@ -3,13 +3,18 @@ import org.json.simple.*;
 public class Testing {
     
     public static void main(String[] args){
+        stestirajJSONSporocila();
+    }
+    
+    public static void stestirajJSONSporocila(){
         Message sporocilo = new Message("public", "meow", "this is a test message.");
-
+    
         System.out.printf("%s\n", sporocilo);
-
+    
         Message novoSporocilo = new Message("public", "hacker", "you've been pwned!!!");
         JSONObject leteceSporocilo = novoSporocilo.toJson();
         Message prejetoSporocilo = new Message(leteceSporocilo);
         System.out.printf("%s\n", prejetoSporocilo);
+
     }
 }
