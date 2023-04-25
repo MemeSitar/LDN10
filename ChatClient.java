505020b97efd376dc7c5d370ac3e736b65fd63d9
MemeSitar <jurij.sitar@mjau.si>
2023-04-25 12:56:21 +0200

    Method for user input
    Put private message logic into a separate method.

--- ChatClient.java
+++ ChatClient.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class ChatClient extends Thread
 {
 	protected int serverPort = 8888;
+	private String username;
 
 
 	public static void main(String[] args) throws Exception {
@@ -23,6 +24,8 @@ public class ChatClient extends Thread
 		Socket socket = null;
 		DataInputStream in = null;
 		DataOutputStream out = null;
+		this.username = username;
+		ChatClientMessageReceiver message_receiver = null;
 
 		// connect to the chat server
 		try {
@@ -36,7 +39,7 @@ public class ChatClient extends Thread
 			
 			System.out.println("[system] connected");
 
-			ChatClientMessageReceiver message_receiver = new ChatClientMessageReceiver(in); // create a separate thread for listening to messages from the chat server
+			message_receiver = new ChatClientMessageReceiver(in); // create a separate thread for listening to messages from the chat server
 			message_receiver.start(); // run the new thread
 		} catch (Exception e) {
 			e.printStackTrace(System.err);
@@ -45,40 +48,47 @@ public class ChatClient extends Thread
 
 		// read from STDIN and send messages to the chat server
 		BufferedReader std_in = new BufferedReader(new InputStreamReader(System.in));
-		String userInput;
-		while ((userInput = std_in.readLine()) != null) { // read a line from the console
 
-			if (userInput.length() > 1 && userInput.substring(0, 1).equals("@")
-				 && userInput.indexOf("[") != -1 && userInput.indexOf("]") != -1){ 
-				// if message is private
-				// also checks that other strings still come through
-				int endOfUsername = userInput.indexOf("]");
-				String receiver = userInput.substring(userInput.indexOf("[") + 1, endOfUsername);
-
-				//! THIS IS HERE FOR DEBUGGING
-				//*System.out.printf("THIS IS A PRIVATE MESSAGE\n");
-				//*System.out.printf("THE RECEIVER IS: %s\n", receiver);
-
-				// catches out of bounds exceptions.
-				try {
-					Message message = new Message("PRIVATE",
-					 username, receiver, userInput.substring(endOfUsername + 1));
-					this.sendMessage(message, out);
-				} catch (Exception e) {
-					e.printStackTrace(System.err);
-				}
-
-			} else { // message is public
-				Message message = new Message("PUBLIC", username, null, userInput);
-				this.sendMessage(message, out);
-			}
-		}
-
+		this.readUserInput(std_in, out);
+		
 		// cleanup
 		out.close();
 		in.close();
 		std_in.close();
 		socket.close();
+		System.exit(0);
+	}
+	
+	private void readUserInput(BufferedReader std_in, DataOutputStream out) throws IOException{
+		String userInput;
+		while ((userInput = std_in.readLine()) != null) { // read a line from the console
+			if (userInput.equals("exit()")){
+				return;
+			} else if (userInput.length() > 1 && userInput.substring(0, 1).equals("@")
+				 && userInput.indexOf("[") != -1 && userInput.indexOf("]") != -1){ 
+				// if message is private
+				// also checks that other strings still come through
+				int endOfUsername = userInput.indexOf("]");
+				String receiver = userInput.substring(userInput.indexOf("[") + 1, endOfUsername);
+	
+				//! THIS IS HERE FOR DEBUGGING
+				//*System.out.printf("THIS IS A PRIVATE MESSAGE\n");
+				//*System.out.printf("THE RECEIVER IS: %s\n", receiver);
+	
+				// catches out of bounds exceptions.
+				try {
+					Message message = new Message("PRIVATE",
+					 this.username, receiver, userInput.substring(endOfUsername + 1));
+					this.sendMessage(message, out);
+				} catch (Exception e) {
+					e.printStackTrace(System.err);
+				}
+	
+			} else { // message is public
+				Message message = new Message("PUBLIC", username, null, userInput);
+				this.sendMessage(message, out);
+			}
+		}
 	}
 
 	private void sendMessage(Message message, DataOutputStream out) {
@@ -109,9 +119,15 @@ class ChatClientMessageReceiver extends Thread {
 				System.out.printf("%s", message); // print the message to the console
 			}
 		} catch (Exception e) {
-			System.err.println("[system] could not read message");
-			e.printStackTrace(System.err);
-			System.exit(1);
+			if (e instanceof SocketException){
+				//! for debugging purposes.
+				//*System.err.println("[system] socket closed outside of thread.");
+				return;
+			} else {
+				System.err.println("[system] could not read message");
+				e.printStackTrace(System.err);
+				System.exit(1);
+			}
 		}
 	}
 }
